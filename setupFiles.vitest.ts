bbd9bccf4fa1636332c19ba56d4705f2160e9a31
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-04-02 22:19:59 +0700

    refactor: setup reflection for vitest

---
+++ setupFiles.vitest.ts
@@ -0,0 +1,3 @@
+// tsyringe
+// look https://github.com/microsoft/tsyringe/issues/180#issuecomment-982651609
+import '@abraham/reflection'
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+// tsyringe
+// look https://github.com/microsoft/tsyringe/issues/180#issuecomment-982651609
+import '@abraham/reflection'
+
 import './assets/main.scss'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-
-// tsyringe
-import 'reflect-metadata'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
