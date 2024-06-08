19fdec473c9f857086a8ea34b0107b820e49545a
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-06-08 13:42:16 +0700

    feat: add singly linked list visualization

---
+++ src/components/DataStructure/LinkedItem.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup generic="TData">
+import { VIcon } from '@/components/VIcon'
+import type { NodeItem } from '@/models/SinglyLinkedList'
+
+defineProps<{
+  data?: NodeItem<TData>
+}>()
+</script>
+
+<template>
+  <div class="tag is-large is-justify-content-space-between">
+    <span>{{ data?.getData() }}</span>
+    <template v-if="data?.hasNext()">
+      <VIcon class="ml-4">
+        <i class="bi bi-arrow-right"></i>
+      </VIcon>
+    </template>
+  </div>
+
+  <template v-if="data?.hasNext()">
+    <LinkedItem :data="data.getNext() ?? undefined" />
+  </template>
+</template>
--- src/components/DataStructure/LinkedList.vue
+++ src/components/DataStructure/LinkedList.vue
@@ -1,14 +1,47 @@
 <script lang="ts" setup>
+import { VButton } from '@/components/VButton'
+import { VTextField } from '@/components/VTextField'
 import { SinglyLinkedList } from '@/models/SinglyLinkedList'
+import { computed } from 'vue'
+import LinkedItem from './LinkedItem.vue'
 
 const list = new SinglyLinkedList<string>()
 list.append('fish')
+list.append('dog')
+list.append('giraffe')
+list.append('shark')
+list.append('duck')
+list.append('bird')
+list.append('eagle')
+list.append('crab')
+
+const head = computed(() => list.getFirst())
 </script>
 
 <template>
   <section class="section">
-    <pre>
-        {{ list }}
-    </pre>
+    <div class="container is-max-desktop block">
+      <div class="block">
+        <h1 class="title">Linked List</h1>
+      </div>
+      <div class="box block">
+        <form>
+          <VTextField label="Data" />
+          <div class="field">
+            <div class="control">
+              <VButton colors="primary">Insert</VButton>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="container block is-max-desktop">
+      <div class="block">
+        <h1 class="title is-5">Visualization</h1>
+      </div>
+      <div class="block grid">
+        <LinkedItem v-if="head" :data="head"> </LinkedItem>
+      </div>
+    </div>
   </section>
 </template>
