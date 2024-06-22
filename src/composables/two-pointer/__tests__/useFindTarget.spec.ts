724dee73662f8f2cce60762730ecd703e90c5f82
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-06-22 14:38:17 +0700

    test: fix test

--- src/composables/two-pointer/__tests__/useFindTarget.spec.ts
+++ src/composables/two-pointer/__tests__/useFindTarget.spec.ts
@@ -3,12 +3,10 @@ import { useFindTarget } from '../useFindTarget'
 
 describe('useFindTarget', () => {
   it('should return value', () => {
-    const arr = [3, 4, 1, 2, 0, 5]
-    const { find, sort } = useFindTarget(arr, 7)
+    const arr = [3, 4, 1, 2, 0, 5].sort()
+    const { find } = useFindTarget()
 
-    sort()
-
-    const res = find()
+    const res = find(arr, 7)
 
     expect(res).toEqual([2, 5])
   })
