933682e4dbda04e361dc6ec62e0ab90e441a06e1
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-06-28 23:24:50 +0700

    fix(binary search): fixed mid count

--- src/utils/BinarySearch.ts
+++ src/utils/BinarySearch.ts
@@ -6,22 +6,28 @@
  * 2. contstant time of access O(1) in any data structure
  */
 export class BinarySearch {
-  static search(arr: number[], k: number) {
-    let left = 0
-    let right = arr.length - 1
+  static search(arr: number[], k: number): number {
+    let left = 0 // left pointer
+    let right = arr.length - 1 // right most pointer
 
-    while (left < right) {
-      const mid = Math.floor((right - left) / 2)
+    while (left <= right) {
+      // count mid index on each iteration
+      const mid = Math.floor((left + right) / 2)
 
+      // if mid value less than target value (k)
       if (arr[mid] < k) {
+        // move left pointer to the mid + 1
         left = mid + 1
       } else if (arr[mid] > k) {
+        // else move right pointer to the mid - 1
         right = mid - 1
       } else {
-        return true
+        // else should be arr[mid] equal with target value (k)
+        return arr[mid]
       }
     }
 
-    return false
+    // return the correct value
+    return arr[left]
   }
 }
--- src/utils/__test__/BinarySearch.spec.ts
+++ src/utils/__test__/BinarySearch.spec.ts
@@ -6,8 +6,8 @@ describe('Binary Search', () => {
     const arr = [8, 1, 3, 2, 5, 0, 9]
     arr.sort()
 
-    const result = BinarySearch.search(arr, 9)
+    const result = BinarySearch.search(arr, 10)
 
-    expect(result).toBe(true)
+    expect(result).toBe(10)
   })
 })
