07472eb2dbe0f1c094717e410cba1d2c577190b1
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-07-11 19:02:12 +0700

    feat(LL): add brief description

--- src/components/data-structure/LinkedList.vue
+++ src/components/data-structure/LinkedList.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
 import { VButton } from '@/components/ui/VButton'
 import { VControl } from '@/components/ui/VControl'
+import { VEmpty } from '@/components/ui/VEmpty'
 import { VField } from '@/components/ui/VField'
+import * as VHero from '@/components/ui/VHero'
 import { VTextField } from '@/components/ui/VTextField'
 import { SinglyLinkedList } from '@/models/linked-list/SinglyLinkedList'
 import { toTypedSchema } from '@vee-validate/yup'
@@ -39,15 +41,41 @@ const clear = () => {
 </script>
 
 <template>
+  <VHero.VHero variant="primary">
+    <VHero.VHeroBody>
+      <div class="container">
+        <h1 class="title">Linked List</h1>
+        <h3 class="subtitle">
+          Linked List is a linear collection of data elements whose order is not given by their
+          physical placement in memory. Instead, each elements points to the next.
+        </h3>
+        <div class="content">
+          <p>Characteristics</p>
+          <ol>
+            <li>
+              Data structure consisting of a collection of nodes which together represent a sequence
+            </li>
+            <li>Contain two fields: data and a link to the next node</li>
+            <li>Simplest and most common data structures</li>
+            <li>
+              Can be easily inserted or removed without reallocation or reorganization of the entire
+              structure
+            </li>
+          </ol>
+        </div>
+      </div>
+    </VHero.VHeroBody>
+  </VHero.VHero>
   <section class="section">
     <div class="container is-max-desktop block">
-      <div class="block">
-        <h1 class="title">Linked List</h1>
-        <h3 class="subtitle">This page show you how singly linked list works</h3>
-      </div>
       <div class="box block">
         <form @submit="onSubmit">
-          <VTextField v-model="formState.data" name="data" label="Data" />
+          <VTextField
+            v-model="formState.data"
+            placeholder="Type anything as data, for example Fish"
+            name="data"
+            label="Data"
+          />
           <VField is-grouped>
             <template #grouped>
               <VControl>
@@ -70,6 +98,7 @@ const clear = () => {
       </div>
       <div class="block grid">
         <LinkedItem v-if="head" :data="head" />
+        <VEmpty v-else />
       </div>
     </div>
   </section>
